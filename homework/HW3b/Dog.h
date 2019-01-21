99d4f20837222aad938dc828b2ea5b86eb6f6150
Andrew St. Pierre <astpier@purdue.edu>
2019-01-21 16:31:18 -0500

    adding hw3

---
+++ homework/HW3b/Dog.h
@@ -0,0 +1,19 @@
+//
+// Created by asaintp on 1/21/19.
+//
+
+#ifndef HW3B_DOG_H
+#define HW3B_DOG_H
+
+#include "Duck.h"
+
+class Dog {
+public:
+
+    Dog( );
+    virtual ~Dog( );
+    void play(Duck*);
+};
+
+
+#endif //HW3B_DOG_H
