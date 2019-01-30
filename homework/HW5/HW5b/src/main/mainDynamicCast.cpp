9bd7d186ef680e7a3c05d3aeda78fc26ed5438fd
Andrew St. Pierre <astpier@purdue.edu>
2019-01-30 11:56:58 -0500

    hw5 complete

--- homework/HW5/HW5b/src/main/mainDynamicCast.cpp
+++ homework/HW5/HW5b/src/main/mainDynamicCast.cpp
@@ -1,8 +1,3 @@
-//
-// THIS CODE IS EVIL and you should never do anything like this.
-// It demonstrates what can go wrong when you use C-style casts
-//
-
 #include "src/lib/Person.h"
 #include "src/lib/Student.h"
 #include "src/lib/Faculty.h"
@@ -15,21 +10,21 @@ int main() {
     Student* student;
     void* v;
 
-    student = (Student*) faculty;
+    student = dynamic_cast<Student*>(faculty);
     std::cout << "before (student=faculty).putGPA(6.6)" << std::endl;
     student->putGPA(6.6);
     std::cout << "before (student=faculty).getGPA" << std::endl;
     student->getGPA( );
 
     student = new Student(3,3,3,"Irena");
-    faculty = (Faculty *) student; // compiles, but will cause problems at runtime
+    faculty = dynamic_cast<Faculty *>(student); // compiles, but will cause problems at runtime
     std::cout << "before (faculty=student).putSalary" << std::endl;
     faculty->putSalary(500.0);
     std::cout << "before (faculty=student).getSalary" << std::endl;
     faculty->getSalary( );
 
     v = (void*) person ;
-    student = (Student*) v;
+    student = dynamic_cast<Student*>(v);
     std::cout << "before (student=person).getGPA" << std::endl;
     student->getGPA( );
     std::cout << "finished" << std::endl;
