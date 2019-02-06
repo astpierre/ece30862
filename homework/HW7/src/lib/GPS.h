af58b578c672bdcfe50c1fb89f5d2ddb3c47860b
Andrew St. Pierre <astpier@purdue.edu>
2019-02-06 18:06:20 -0500

    started hw7

---
+++ homework/HW7/src/lib/GPS.h
@@ -0,0 +1,22 @@
+#ifndef GPS_H
+#define GPS_H
+#include "Observer.h"
+#include "Subject.h"
+
+class GPS: public Subject {
+public:
+   GPS( );
+   virtual ~GPS( );
+   virtual void subscribe(Observer *);
+   virtual void update(double, double);
+   virtual void notify();
+private:
+    double x, y;
+    int idx;
+    int size;
+    Observer** obs;
+};
+
+
+
+#endif /* GPS_H */
